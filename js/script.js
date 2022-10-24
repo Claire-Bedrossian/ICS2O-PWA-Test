5ded19c6cb43c5d1e9ea1e11123ceb8f56bf90b2
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-24 13:42:36 -0400

    Fixed names

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 202X Claire Bedrossian All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Claire Bedrossian
+// Created on: XXX 202X
 // This file contains the JS functions for index.html
 
 "use strict"
